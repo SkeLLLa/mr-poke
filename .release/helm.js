cc4e27ea443cec02dab89df2d7ed162205b01aff
Alex <2273103+SkeLLLa@users.noreply.github.com>
2023-10-06 22:11:42 +0200

    fix: use different name for helm chart reg

--- .release/helm.js
+++ .release/helm.js
@@ -2,6 +2,6 @@ module.exports = [
   'semantic-release-helm3',
   {
     chartPath: './helm/chart',
-    registry: `oci://ghcr.io/skellla/mr-poke`,
+    registry: `oci://ghcr.io/skellla/charts`,
   },
 ];
